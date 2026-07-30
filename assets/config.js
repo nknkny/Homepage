c6ed59ac545288be7321552e90f8b435ed4072dc
nknkny <166302688+nknkny@users.noreply.github.com>
2026-07-30 17:17:10 +0900

    Temporarily pause API until login fix is deployed

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.AKIYA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzWYTf9K_XyEfo1CD4Pshg18ANH5_zhOsTnyiFhKTwbDgs17ZOXqLgAxKrvO7TA3wYr0w/exec",
+  API_URL: "",
   SITE_NAME: "空き家レスキュー",
   SERVICE_AREA: "青森市",
   SPREADSHEET_ID: "12nRr3Fkq8tYZWd-MQvdQXpeO9p9cGuT3CvtRnL_omw8",
